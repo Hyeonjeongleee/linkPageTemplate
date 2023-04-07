ebcbc714ea3f29ce7a3fe884b1eed572e71532d4
someone <someone@someplace.com>
2023-04-07 16:16:44 +0900

    some init msg

---
+++ script.js
@@ -0,0 +1,6 @@
+<div class="container">
+  <div class="box">
+    <button onclick="window.location.href='https://www.instagram.com';">좋아하는 것</button>
+    <img class="image" src="images/image 5.png" alt="image 5">
+  </div>
+</div>
